cb877e1e3cb84e1c64b626444e77f1e60629c43b
Lior Rabinovich <lior.px@gmail.com>
2020-02-10 21:44:36 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello wrold');
+    res.end();
+});
+
+server.listen(port);
